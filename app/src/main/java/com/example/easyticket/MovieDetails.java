63151011947766c6d9680a254209a4c0e363c2aa
ThilaaaRlz <twansekara@gmail.com>
2020-10-04 13:40:16 +0530

    Thilanka update

---
+++ app/src/main/java/com/example/easyticket/MovieDetails.java
@@ -0,0 +1,43 @@
+package com.example.easyticket;
+
+public class MovieDetails {
+    private int name;
+    private int date;
+    private  int time;
+    private int movieName;
+
+    public MovieDetails() {
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(int movieName) {
+        this.movieName = movieName;
+    }
+}
